f4b05a6016979ad5e40ef0f29f34956e41b09e72
Akshay <worklikeakshay@gmail.com>
2023-08-13 23:55:48 +0530

    First commit

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+const {Server} = require('socket.io');
+
+const io = new Server();
+const app = express();
+
+app.use(bodyParser.json());
+
+io.on('connection', (socket) =>{});
+
+app.listenerCount(8000, () => console.log('http server running at PORT 8000'));
+io.listen(8001);

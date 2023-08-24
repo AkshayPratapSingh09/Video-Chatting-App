fde2dc693eb5e0e0fe521e57d23a39567854d32e
Akshay Pratap Singh <99736940+AkshayPratapSingh09@users.noreply.github.com>
2023-08-24 23:59:30 +0530

    cr23

--- server/index.js
+++ server/index.js
@@ -11,6 +11,19 @@ io.on('connection', (socket) =>{});
 
 app.listenerCount(8000, () => console.log('http server running at PORT 8000'));
 io.listen(8001);
+const bodyParser = require('body-parser');
+const express = require('express');
+const {Server} = require('socket.io');
+
+const io = new Server();
+const app = express();
+
+app.use(bodyParser.json());
+
+io.on('connection', (socket) =>{});
+
+app.listenerCount(8000, () => console.log('http server running at PORT 8000'));
+io.listen(8001);
 
 const react = require("react");
 const react = require("react");
